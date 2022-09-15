76e6c7d74e3d63b4324562b2faab7350c8a17d89
quiquelhappy <quiquelhappy@gmail.com>
2022-09-15 21:25:35 +0200

    build

--- 0.2/cmake-build-debug/cmake_install.cmake
+++ 0.2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/happy/Documents/GitHub/uab/uab-lp/0.1
+# Install script for directory: C:/Users/happy/Documents/GitHub/uab/uab-lp/0.2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/happy/Documents/GitHub/uab/uab-lp/0.1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/happy/Documents/GitHub/uab/uab-lp/0.2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
