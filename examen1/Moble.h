35caf8e9137369b76774722ec9b64fa67c9ae711
quiquelhappy <quiquelhappy@gmail.com>
2022-10-19 18:27:13 +0200

    base moble

---
+++ examen1/Moble.h
@@ -0,0 +1,20 @@
+//
+// Created by happy on 19/10/2022.
+//
+
+#ifndef EXAMEN1_MOBLE_H
+#define EXAMEN1_MOBLE_H
+
+
+class Moble
+{
+public:
+    Moble();
+    ~Moble() {}
+private:
+    int m_codi;
+    float m_volum;
+};
+
+
+#endif //EXAMEN1_MOBLE_H
