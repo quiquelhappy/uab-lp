0c9b88eb1e6af5b47cfd8157ea176610c18ec963
quiquelhappy <quiquelhappy@gmail.com>
2022-10-19 18:58:35 +0200

    constructor de copia y cout

--- examen1/Moble.h
+++ examen1/Moble.h
@@ -5,14 +5,26 @@
 #ifndef EXAMEN1_MOBLE_H
 #define EXAMEN1_MOBLE_H
 
+#include <iostream>
+
+using namespace std;
+
 
 class Moble
 {
 public:
     Moble(int codi, float volum): m_codi(codi), m_volum(volum)  {};
+    Moble(Moble& m) = default;
+
     ~Moble() {}
     float Volum() const { return m_volum; }
     int Codi() const { return m_codi; }
+
+    friend ostream& operator << (ostream out, const Moble& m) {
+        out << "Codi: " << m.m_codi << ", Volum: " << m.m_volum;
+        return out;
+    }
+
 private:
     int m_codi;
     float m_volum;
